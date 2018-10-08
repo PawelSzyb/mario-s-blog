d61c6e4a00b5deed22be295187180ac167b36234
Paweł Szybaj <pawelszybaj@wp.pl>
2018-10-08 22:41:01 +0200

    created redux setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
@@ -13,18 +16,20 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route exact path="/project/:id" component={ProjectDetails} />
-            <Route path="/signin" component={SignIn} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/create" component={CreateProject} />
-          </Switch>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/project/:id" component={ProjectDetails} />
+              <Route path="/signin" component={SignIn} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/create" component={CreateProject} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers/index";
+
+const middleware = [];
+
+const store = createStore(rootReducer, applyMiddleware(...middleware));
+
+export default store;
