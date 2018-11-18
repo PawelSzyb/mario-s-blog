9632f5c077197acaa002327327f5f6c67710db55
Paweł Szybaj <pawelszybaj@wp.pl>
2018-11-18 23:08:05 +0100

    Syncing Data with Firestore

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 import Notification from "./Notifications";
 import ProjectList from "../projects/ProjectList";
@@ -22,8 +24,13 @@ export class Dashboard extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  projects: state.project.projects
-});
+const mapStateToProps = state => {
+  return {
+    projects: state.firestore.ordered.projects
+  };
+};
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "projects" }])
+)(Dashboard);
--- src/store.js
+++ src/store.js
@@ -1,24 +1,21 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers/index";
 import thunk from "redux-thunk";
-import { reduxFirestore, getFirebase } from "react-redux-firebase";
-import { reactReduxFirebase, getFirestore } from "redux-firestore";
-import { compose } from "redux";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
 import fbConfig from "./config/fb";
-
-const middleware = [
-  thunk.withExtraArgument({
-    getFirebase,
-    getFirestore
-  })
-];
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(...middleware),
-    reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+    applyMiddleware(
+      thunk.withExtraArgument({
+        getFirebase,
+        getFirestore
+      })
+    ),
+    reactReduxFirebase(fbConfig), // redux binding for firebase
+    reduxFirestore(fbConfig) // redux bindings for firestore
   )
 );
 
